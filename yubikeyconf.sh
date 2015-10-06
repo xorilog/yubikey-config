e1d91fe184c8bbe57494f3dad08e9811f970ca97
Christophe Boucharlat <garfieldounet+github@gmail.com>
2015-10-06 10:50:41 +0200

    added the lines + install of yubico manager

---
+++ yubikeyconf.sh
@@ -0,0 +1,5 @@
+echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120"' > /etc/udev/rules.d/70-u2f.rules
+apt-add-repository ppa:yubico/stable
+apt-get update
+apt-get install yubikey-neo-manager
+echo "you shall reboot now ..."
