537a5f85568c1a447fe7f8758029ee015569c34c
Christophe Boucharlat <christophe.boucharlat@surycat.com>
2015-10-08 14:36:45 +0200

    added Yubikey Standard vendorID

--- yubikeyconf.sh
+++ yubikeyconf.sh
@@ -1,4 +1,4 @@
-echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120"' > /etc/udev/rules.d/70-u2f.rules
+echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0113|0114|0115|0116|0120"' > /etc/udev/rules.d/70-u2f.rules
 apt-add-repository ppa:yubico/stable
 apt-get update
 apt-get install yubikey-neo-manager
