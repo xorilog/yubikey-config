b0b4bf0ef3186ea7fc6df64c984e744f5979d6b6
Christophe Boucharlat <christophe.boucharlat@surycat.com>
2015-10-08 14:48:05 +0200

    added lubif2-host rules

--- yubikeyconf.sh
+++ yubikeyconf.sh
@@ -1,4 +1,20 @@
-echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0113|0114|0115|0116|0120"' > /etc/udev/rules.d/70-u2f.rules
+echo '#KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0113|0114|0115|0116|0120"
+# this udev file should be used with udev 188 and newer
+ACTION!="add|change", GOTO="u2f_end"
+
+# Yubico YubiKey
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
+
+# Happlink (formaly Plug-Up) Security KEY
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="f1d0", TAG+="uaccess"
+
+#  Neowave Keydo
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1e0d", ATTRS{idProduct}=="f1d0", TAG+="uaccess"
+
+# HyperSecu HyperFIDO
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="096e", ATTRS{idProduct}=="0880", TAG+="uaccess"
+
+LABEL="u2f_end"' > /etc/udev/rules.d/70-u2f.rules
 apt-add-repository ppa:yubico/stable
 apt-get update
 apt-get install yubikey-neo-manager
